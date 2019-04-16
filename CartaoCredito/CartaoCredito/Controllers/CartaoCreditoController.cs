f6493c7068e86baa0c05519bfd3d32c05dd569c8
Luiz Barros <figueiredobarros@gmail.com>
2019-04-16 20:20:34 -0300

    api cartao credito

---
+++ CartaoCredito/CartaoCredito/Controllers/CartaoCreditoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartaoCredito.Controllers
+{
+    [Route("api/CartaoCredito")]
+    [ApiController]
+    public class CartaoCreditoController : ControllerBase
+    {
+
+        [Route("cartao")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> ValidarCartao([FromQuery] string dadosCartao)
+        {
+            return Ok();
+        }
+
+        [Route("cartao/compra")]
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> RealizarTransacao([FromQuery] string dadosCartao)
+        {
+            return Ok();
+        }
+
+    }
+}
