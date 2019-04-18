54546bd79fbd8f51947a561609f8a27ff09ee2cd
Luiz Barros <figueiredobarros@gmail.com>
2019-04-18 00:16:44 -0300

    chamada de apis. falta testar

--- CartaoCredito/CartaoCredito/Controllers/CartaoCreditoController.cs
+++ CartaoCredito/CartaoCredito/Controllers/CartaoCreditoController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using CartaoCredito.Extension;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,20 +13,36 @@ namespace CartaoCredito.Controllers
     [ApiController]
     public class CartaoCreditoController : ControllerBase
     {
-
-        [Route("cartao")]
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> ValidarCartao([FromQuery] string dadosCartao)
+        private static readonly HttpClient client = new HttpClient();
+        public CartaoCreditoController()
         {
-            return Ok();
+            client.BaseAddress = new System.Uri(@"https://localhost:5001/");
         }
-
         [Route("cartao/compra")]
         [HttpPost]
-        public ActionResult<IEnumerable<string>> RealizarTransacao([FromQuery] string dadosCartao)
+        public ActionResult<IEnumerable<string>> RealizarTransacao([FromQuery] Cartao cartao)
         {
-            return Ok();
+            ValidarCartao(cartao);
+            //Transacao efetuada...
+            return Ok("Transação efetuada com sucesso.");
         }
 
+        
+
+        private void ValidarCartao(Cartao cartao)
+        {
+            if (cartao.CodigoVerificador == default(int) || cartao.Numerocartao == default(int))
+                throw new ArgumentException("Preencha os dados do Cartao!");
+        }
+    }
+
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+    public class Cartao {
+        public int Numerocartao { get; set; }
+        public int CodigoVerificador { get; set; }
     }
 }
